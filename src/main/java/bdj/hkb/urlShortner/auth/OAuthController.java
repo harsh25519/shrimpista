9cf4ac66466cad4658eb0e41e25a445a41ffeaaa
HarshBhardwaj <harshbdj25@gmail.com>
2026-08-01 21:26:52 +0530

    Some changes in redirection

--- src/main/java/bdj/hkb/urlShortner/auth/OAuthController.java
+++ src/main/java/bdj/hkb/urlShortner/auth/OAuthController.java
@@ -4,7 +4,9 @@ import bdj.hkb.urlShortner.auth.dto.AuthResponse;
 import bdj.hkb.urlShortner.auth.dto.OAuthProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -35,6 +37,26 @@ public class OAuthController {
 
         AuthResponse tokens = oAuthClientService.exchangeCode(code);
 
-        return ResponseEntity.ok(tokens);
+        ResponseCookie accessCookie = ResponseCookie.from("accessToken", tokens.accessToken())
+                .httpOnly(true)
+                .secure(true)
+                .path("/")
+                .maxAge(15 * 60)
+                .sameSite("None")
+                .build();
+
+        ResponseCookie refreshCookie = ResponseCookie.from("refreshToken", tokens.refreshToken())
+                .httpOnly(true)
+                .secure(true)
+                .path("/")
+                .maxAge(7 * 24 * 60 * 60)
+                .sameSite("None")
+                .build();
+
+        return ResponseEntity.status(HttpStatus.FOUND)
+                .header(HttpHeaders.SET_COOKIE, accessCookie.toString())
+                .header(HttpHeaders.SET_COOKIE, refreshCookie.toString())
+                .header(HttpHeaders.LOCATION,  frontend + "/dashboard")
+                .build();
     }
 }
